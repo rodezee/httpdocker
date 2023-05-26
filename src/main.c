fa3383d57aedfa6fc9382e928fb0ff688fd382e7
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-26 18:55:29 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,11 +4,32 @@
 
 static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
   if (ev == MG_EV_HTTP_MSG) {
-    mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "Hello, %s\n", "world");
+    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+    if (mg_http_match_uri(hm, "/api/sum")) {
+      // Expecting JSON array in the HTTP body, e.g. [ 123.38, -2.72 ]
+      double num1, num2;
+      if (mg_json_get_num(hm->body, "$[0]", &num1) &&
+          mg_json_get_num(hm->body, "$[1]", &num2)) {
+        // Success! create JSON response
+        mg_http_reply(c, 200, "Content-Type: application/json\r\n",
+                      "{%m:%g}\n",
+                      mg_print_esc, 0, "result", num1 + num2);
+      } else {
+        mg_http_reply(c, 500, NULL, "Parameters missing\n");
+      }
+    } else {
+      mg_http_reply(c, 500, NULL, "\n");
+    }
   }
 }
 
 /*
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+  if (ev == MG_EV_HTTP_MSG) {
+    mg_http_reply(c, 200, "Content-Type: text/plain\r\n", "Hello, %s\n", "world");
+  }
+}
+
 int main(int argc, char *argv[]) {
   struct mg_mgr mgr;
   mg_mgr_init(&mgr);                                        // Init manager
