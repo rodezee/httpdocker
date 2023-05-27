7647ebaf59e01fec05962470a1d8a16bd84ebb64
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-27 14:41:59 +0200

    test

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
           if (response == CURLE_OK)
           {
             char *dbuf = docker_buffer(docker);
-            if ( startsWith("No such image:", dbuf) ) {
+            if ( startsWith(dbuf, "No such image:") ) {
               mg_http_reply(c, 200, "Content-Type: application/json\r\n",
                             "{%m:%s}\n",
                             mg_print_esc, 0, "result", "You need to pull first!");
