b8cbead17e26bb1c4e799efc07f4f9d339539191
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-30 19:19:22 +0200

    test

--- src/main.c
+++ src/main.c
@@ -7,11 +7,55 @@
 
 // BEGIN extra C functions
 
-bool startsWith(const char *pre, const char *str)
+bool starts_with(const char *pre, const char *str)
 {
     size_t lenpre = strlen(pre),
            lenstr = strlen(str);
     return lenstr < lenpre ? false : memcmp(pre, str, lenpre) == 0;
+}
+
+char * str_slice(char str[], int slice_from, int slice_to)
+{
+    // if a string is empty, returns nothing
+    if (str[0] == '\0')
+        return NULL;
+
+    char *buffer;
+    size_t str_len, buffer_len;
+
+    // for negative indexes "slice_from" must be less "slice_to"
+    if (slice_to < 0 && slice_from < slice_to) {
+        str_len = strlen(str);
+
+        // if "slice_to" goes beyond permissible limits
+        if (abs(slice_to) > str_len - 1)
+            return NULL;
+
+        // if "slice_from" goes beyond permissible limits
+        if (abs(slice_from) > str_len)
+            slice_from = (-1) * str_len;
+
+        buffer_len = slice_to - slice_from;
+        str += (str_len + slice_from);
+
+    // for positive indexes "slice_from" must be more "slice_to"
+    } else if (slice_from >= 0 && slice_to > slice_from) {
+        str_len = strlen(str);
+
+        // if "slice_from" goes beyond permissible limits
+        if (slice_from > str_len - 1)
+            return NULL;
+
+        buffer_len = slice_to - slice_from;
+        str += slice_from;
+
+    // otherwise, returns NULL
+    } else
+        return NULL;
+
+    buffer = calloc(buffer_len, sizeof(char));
+    strncpy(buffer, str, buffer_len);
+    return buffer;
 }
 
 // END extra C functions
@@ -45,7 +89,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
             fprintf(stderr, "Try to create container, CURL response code: %d\n", (int) responseCreate);
             char *dbuf = docker_buffer(docker);
             fprintf(stderr, "dbuf: %s\n", dbuf);
-            if ( startsWith("{\"message\":\"No such image: ", dbuf) ) { // image needs to be pulled
+            if ( starts_with("{\"message\":\"No such image: ", dbuf) ) { // image needs to be pulled
               // mg_http_reply(c, 200, "Content-Type: application/json\r\n",
               //               "{%m:%s}\n",
               //               mg_print_esc, 0, "result", dbuf);
@@ -65,10 +109,11 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
               }
 
             } else { // image already on the server
+              char *id = str_slice( dbuf, 17, (17+64) );
               mg_http_reply(c, 200, "Content-Type: application/json\r\n",
                             "{%m:%s}\n",
-                            mg_print_esc, 0, "result", dbuf);
-              fprintf(stderr, "Image was already there, CURL response code: %d\n", (int) responseCreate);
+                            mg_print_esc, 0, "id", id);
+              fprintf(stderr, "Image found, container created, id: %d\n", (int) responseCreate);
             }
           } else {
             mg_http_reply(c, 200, "Content-Type: application/json\r\n",
