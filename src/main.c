d1013077236fafec52d1c21649d5dcd1ee783d4c
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-31 15:51:45 +0200

    test

--- src/main.c
+++ src/main.c
@@ -145,7 +145,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                 responseWait = docker_post(docker, cmd_url_wait, "");
                 if (responseWait == CURLE_OK) {
                   char *dbuf = docker_buffer(docker);
-                  if ( strcmp(dbuf, "-{\"StatusCode\":0}") == 0 ) {
+                  if ( strcmp(dbuf, "{\"StatusCode\":0}") == 0 ) {
                     mg_http_reply(c, 200, "Content-Type: application/json\r\n",
                                   "{%m:\"%s\"}\n",
                                   mg_print_esc, 0, "Container Waited Successfully, id", id);
@@ -153,8 +153,9 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                   } else {
                     mg_http_reply(c, 200, "Content-Type: application/json\r\n",
                                   "{%m:\"%s\"}\n",
-                                  mg_print_esc, 0, "Container did not become ready during waiting process, id", id);
-                    fprintf(stderr, "strcmp(dbuf, \"{\"StatusCode\":0}\"): %d\n", strcmp(dbuf, "-{\"StatusCode\":0}"));
+                                  //mg_print_esc, 0, "Container did not become ready during waiting process, id", id);
+                                  mg_print_esc, 0, "Container did not become ready during waiting process, dbuf", dbuf);
+                    fprintf(stderr, "strcmp(dbuf, \"{\"StatusCode\":0}\"): %d\n", strcmp(dbuf, "{\"StatusCode\":0}"));
                     fprintf(stderr, "Container did not become ready during waiting process, dbuf: %s\n", dbuf);
                   }
                 } else {
