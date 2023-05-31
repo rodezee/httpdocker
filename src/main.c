493449690795dc7205926f3494412ec8aae158f5
rodezee <121246195+rodezee@users.noreply.github.com>
2023-05-31 16:52:15 +0200

    test

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
           fprintf(stderr, "Successfully initialized to docker\n");
 
           // CURLcode responseImageCreate = docker_post(docker, "http://v1.43/images/create", "{\"fromImage\": \"alpine\"}");
-          responseCreate = docker_post(docker, "http://v1.25/containers/create", "{\"Image\": \"busybox\", \"Cmd\": [\"echo\", \"hello world\"]}");
+          responseCreate = docker_post(docker, "http://v1.25/containers/create", "{\"Image\": \"busybox:1.35\", \"Cmd\": [\"echo\", \"hello world\"]}");
           if ( responseCreate == CURLE_OK )
           {
             fprintf(stderr, "Try to create container, CURL response code: %d\n", (int) responseCreate);
@@ -97,7 +97,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 
               // PULL
               CURLcode responsePull;
-              responsePull = docker_post(docker, "http://v1.43/images/create?fromImage=busybox", "");
+              responsePull = docker_post(docker, "http://v1.43/images/create?fromImage=busybox:1.35", "");
               if (responsePull == CURLE_OK) {
                 char *dbuf = docker_buffer(docker);
                 mg_http_reply(c, 200, "Content-Type: application/json\r\n",
