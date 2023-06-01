ecb00fca45943bb38d3f91a33c93204ae2a70c1f
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-01 18:09:36 +0200

    test

--- src/main.c
+++ src/main.c
@@ -184,8 +184,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                     const char *response_cp1 = "http://v1.43/containers/";
                     const char *response_cp2 = "/logs?stdout=true";
                     strcpy(cmd_url_response, response_cp1);
-                    // strcat(cmd_url_response, id);
-                    strcat(cmd_url_response, "b999");
+                    strcat(cmd_url_response, id);
                     strcat(cmd_url_response, response_cp2);
                     fprintf(stderr, "response cmd_url_response: %s\n", cmd_url_response);
                     responseResponse = docker_get(docker, cmd_url_response);
