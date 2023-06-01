3936966b188fd0f54fdd2ab64b8b35f43b6d40e5
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-01 19:54:51 +0200

    test

--- src/main.c
+++ src/main.c
@@ -193,7 +193,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                     responseResponse = docker_get(docker, cmd_url_response);
                     if (responseResponse == CURLE_OK) {
                       // char *dbuf = docker_buffer(docker);
-                      char *dbuf = docker->buffer->data;
+                      char *dbuf = &docker->buffer->data;
                       if ( strlen(dbuf) == 1 ) { //"\u0090"
                         mg_http_reply(c, 200, "Content-Type: application/json\r\n",
                                       "{%m:\"%c %d\"}",
