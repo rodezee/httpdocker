5216a6c752563429b38c5cdc7823536ad6b8bdab
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-01 15:52:53 +0200

    test

--- src/main.c
+++ src/main.c
@@ -176,9 +176,9 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                     if (responseResponse == CURLE_OK) {
                       char *dbuf = docker_buffer(docker);
                       mg_http_reply(c, 200, "Content-Type: application/json\r\n",
-                                    "{%m:\"%m\"}",
+                                    "{%m:\"%s\"}",
                                     mg_print_esc, 0, "dbuf", dbuf);
-                      fprintf(stderr, "Container Response Successfully, dbuf: %m\n", dbuf);
+                      fprintf(stderr, "Container Response Successfully, dbuf: %s\n", dbuf);
                     } else {
                       fprintf(stderr, "Unable to get response from container, CURL response code: %d\n", (int) responseResponse);
                     }
