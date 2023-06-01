6540f6fe7a6e368759938f14fbe21a80a5e055f1
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-01 18:13:10 +0200

    test

--- src/main.c
+++ src/main.c
@@ -180,14 +180,14 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
 
                     // RESPONSE
                     CURLcode responseResponse;
-                    char cmd_url_response[255];
-                    const char *response_cp1 = "http://v1.43/containers/";
-                    const char *response_cp2 = "/logs?stdout=true";
-                    strcpy(cmd_url_response, response_cp1);
-                    strcat(cmd_url_response, id);
-                    strcat(cmd_url_response, response_cp2);
+                    // char cmd_url_response[255];
+                    // const char *response_cp1 = "http://v1.43/containers/";
+                    // const char *response_cp2 = "/logs?stdout=true";
+                    // strcpy(cmd_url_response, response_cp1);
+                    // strcat(cmd_url_response, id);
+                    // strcat(cmd_url_response, response_cp2);
                     // test
-                    cmd_url_response = "http://v1.43/images/json";
+                    char *cmd_url_response = "http://v1.43/images/json";
                     // tset
                     fprintf(stderr, "response cmd_url_response: %s\n", cmd_url_response);
                     responseResponse = docker_get(docker, cmd_url_response);
