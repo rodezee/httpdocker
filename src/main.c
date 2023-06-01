141d96cc8c2553de8273d9ddc3bbccb6aaa77f71
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-01 18:12:21 +0200

    test

--- src/main.c
+++ src/main.c
@@ -187,7 +187,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                     strcat(cmd_url_response, id);
                     strcat(cmd_url_response, response_cp2);
                     // test
-                    cmd_url_response = "http://v1.43/images/json"
+                    cmd_url_response = "http://v1.43/images/json";
                     // tset
                     fprintf(stderr, "response cmd_url_response: %s\n", cmd_url_response);
                     responseResponse = docker_get(docker, cmd_url_response);
