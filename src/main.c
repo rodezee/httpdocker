90c1804d26d4744a3ec86f7596f84b2211ac12b7
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-06 18:40:21 +0200

    test

--- src/main.c
+++ src/main.c
@@ -270,7 +270,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
                   mg_http_reply(c, 200, "Content-Type: text/plain; charset=utf-8\r\n", "%m", mg_print_esc, 0, mr.message);
                 } else {
                   fprintf(stderr, "%s, %s\n", mr.message, mr.result);
-                  char *r str_p_to_char_ar(mr.result);
+                  char *r = str_p_to_char_ar(mr.result);
                   mg_http_reply(c, 200, "Content-Type: text/plain; charset=utf-8\r\n", "%m", mg_print_esc, 0, r);
                   // mg_http_reply(c, 200, "Content-Type: application/json\r\n", "{\"result\":%m}", mg_print_esc, 0, r);
                   free(r);
