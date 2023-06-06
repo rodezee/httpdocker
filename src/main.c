3d7e0de75958afb4829fd277af2f1562c5dce855
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-06 17:15:58 +0200

    test

--- src/main.c
+++ src/main.c
@@ -228,7 +228,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
         //mg_http_reply(c, 500, NULL, "Do docker standard stuff\n");
       
         // char *image = "rodezee/hello-world:0.0.1";
-        char *image = "hello-world:linux";
+        char *image = "library/hello-world:latest";
 
         // INIT
         DOCKER *docker = docker_init("v1.43"); // v1.25
