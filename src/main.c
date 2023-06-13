cb26e13e1ea9f5bd2e06c96d1891ecb05b70b696
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-13 16:53:00 +0200

    test

--- src/main.c
+++ src/main.c
@@ -334,8 +334,10 @@ const char *do_docker_create(DOCKER *docker, const char *body) {
   // CREATE docker_post(docker, "http://v1.25/containers/create", "{\"Image\": \"rodezee/hello-world:0.0.1\", \"Cmd\": [\"echo\", \"hello world\"]}");
   fprintf(stderr, "do_docker_create, body: %s\n", body);
   struct mg_str json = mg_str(body);
+  char *tmp = mg_json_get_str(json, "$.Image");
   char image[1024] = "";
-  strcpy(image, mg_json_get_str(json, "$.Image"));
+  strcpy(image, tmp);
+  free(tmp);
 
   CURLcode responseCreate;
   // responseCreate = docker_post(docker, "http://v1.25/containers/create", "{\"Image\": \"rodezee/hello-world:0.0.1\"}");
@@ -530,7 +532,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
         }
         free(image);
       } else { // found no input, go with standard image
-        image = "library/hello-world:latest"; // rodezee/hello-universe:0.0.1 rodezee/hello-world:0.0.1
+        image = "{\"Image\": \"library/hello-world:latest\"}"; // rodezee/hello-universe:0.0.1 rodezee/hello-world:0.0.1
         rr = docker_run(image);
         if ( !rr.success ) {
           fprintf(stderr, "ERROR: unable to run the image %s\n", image);
