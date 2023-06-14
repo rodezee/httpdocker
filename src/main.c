c45239efec1a9c73cf9c9e0fdfbef45fc360e8f4
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-14 17:52:53 +0200

    test

--- src/main.c
+++ src/main.c
@@ -330,7 +330,7 @@ const char *do_docker_pull(DOCKER *docker, char image[]) {
 }
 */
 
-const char *do_docker_create(DOCKER *docker, char *body) {
+const char *do_docker_create(DOCKER *docker, char body[]) {
   // CREATE docker_post(docker, "http://v1.25/containers/create", "{\"Image\": \"rodezee/hello-world:0.0.1\", \"Cmd\": [\"echo\", \"hello world\"]}");
   fprintf(stderr, "do_docker_create, body: %s\n", body);
   struct mg_str json = mg_str(body);
