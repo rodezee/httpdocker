a8e16664748d38d9a3bfc5c315db8b31daa986fc
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-14 17:14:34 +0200

    test

--- src/main.c
+++ src/main.c
@@ -352,7 +352,8 @@ const char *do_docker_create(DOCKER *docker, char *body) {
       if( starts_with("SUCCESS:", dpull) ) {  
         // return do_docker_create_skip_pulling(docker, image); TRY TO CREATE AFTER PULL
         responseCreate = 0;
-        free(dbuf);
+        free(*dbuf);
+        *dbuf = NULL;
         responseCreate = docker_post(docker, "http://v1.25/containers/create", body);
         if ( responseCreate == CURLE_OK ) {
           fprintf(stderr, "Try to create container after pulling image, CURL response code: %d\n", (int) responseCreate);
