5f8c190e4613bdea30659f0a81558b01dc2274f9
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-15 19:07:30 +0200

    test

--- src/main.c
+++ src/main.c
@@ -600,8 +600,8 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
         struct mg_http_serve_opts opts = {0};
         opts.root_dir = s_root_dir;
         opts.ssi_pattern = s_ssi_pattern;
+        fprintf(stderr, "opts.ssi_pattern => %s\n", opts.ssi_pattern);
         mg_http_serve_dir(c, hm, &opts);
-        fprintf(stderr, "opts.ssi_pattern => %s", s_ssi_pattern);
         mg_http_parse((char *) c->send.buf, c->send.len, &tmp);
         cl = mg_http_get_header(&tmp, "Content-Length");
         if (cl == NULL) cl = &unknown;
