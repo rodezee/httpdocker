b296fec220743dfc28d77dbb098d1ba3ab6991be
rodezee <121246195+rodezee@users.noreply.github.com>
2023-06-15 18:36:06 +0200

    test

--- src/main.c
+++ src/main.c
@@ -550,7 +550,7 @@ responseResult docker_run(const char *body) {
 // END DOCKER
 
 static int s_debug_level = MG_LL_INFO;
-static const char *s_root_dir = ".";
+static const char *s_root_dir = "/www";
 static const char *s_listening_address = "http://0.0.0.0:8000";
 static const char *s_enable_hexdump = "no";
 static const char *s_ssi_pattern = "#.html";
