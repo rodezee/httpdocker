87e91a4e67c5072b03251e16cfb77b76b3088f6c
rodezee <121246195+rodezee@users.noreply.github.com>
2023-07-04 18:23:26 +0200

    test

--- src/main.c
+++ src/main.c
@@ -391,8 +391,12 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
           mg_http_reply(c, 200, "Content-Type: application/json\r\n", "{\"error\":%m}", mg_print_esc, 0, rr.response);
         } else {
           fprintf(stderr, "SUCCESS: did run the body %s\n", body);
-          mg_http_reply(c, 200, ct, "%s", mg_print_esc, 0, rr.response);
+          mg_http_reply(c, 200, ct, "%s", rr.response);
           free(rr.response);
+          // char *json = mg_mprintf("{%m:%d}", MG_ESC("name"), 123);
+          // mg_http_reply(c, 200, "Content-Type: application/json\r\n", "%s\n", json);
+          // free(json);
+
         }
       }
     } else if ( mg_http_match_uri(hm, "#.httpd") && mg_casecmp( s_httpd_files_cgi, "yes") == 0 ) {
