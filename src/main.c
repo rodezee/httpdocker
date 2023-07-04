375f3e70ebb6ea7d900c4c9acb1d65b2bfbd76a4
rodezee <121246195+rodezee@users.noreply.github.com>
2023-07-04 18:19:18 +0200

    test

--- src/main.c
+++ src/main.c
@@ -391,7 +391,7 @@ static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
           mg_http_reply(c, 200, "Content-Type: application/json\r\n", "{\"error\":%m}", mg_print_esc, 0, rr.response);
         } else {
           fprintf(stderr, "SUCCESS: did run the body %s\n", body);
-          mg_http_reply(c, 200, ct, "%m", mg_print_esc, 0, rr.response);
+          mg_http_reply(c, 200, ct, "%s", mg_print_esc, 0, rr.response);
           free(rr.response);
         }
       }
